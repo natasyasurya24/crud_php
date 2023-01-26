845467db3dd5cfb65b2fbf218e957b21a64bd5f6
natasyasurya24 <natasyasurya20@gmail.com>
2023-01-26 19:58:34 +0700

    read.php

--- add_data.php
+++ add_data.php
@@ -1,20 +1,20 @@
 <?php
-require_once "config.php";
-if (isset($_POST['submit'])) {
+    require_once "config.php";
+    if(isset($_POST['submit'])){
 
-    $name = $_POST['name'];
-    $grade = $_POST['grade'];
-    $marks = $_POST['marks'];
+        $name = $_POST['name'];
+        $grade = $_POST['grade'];
+        $marks = $_POST['marks'];
 
-    if ($name != "" && $grade != "" && $marks != "") {
-        $sql = "INSERT INTO results ('name', 'class', 'marks') VALUES ('$name', '$grade', $marks)";
-        if (mysqli_query($conn, $sql)) {
-            header("location: form.php");
-        } else {
-            echo "Something went wrong. Please try again later.";
+        if($name != "" && $grade != "" && $marks != "" ){
+            $sql = "INSERT INTO results ( name, grade, marks) VALUES ('$name', '$grade', $marks)";
+            if (mysqli_query($conn, $sql)) {
+                header("location: form.php");
+            } else {
+                 echo "Something went wrong. Please try again later.";
+            }
+        }else{
+            echo "Name, Class and Marks cannot be empty!";
         }
-    } else {
-        echo "Name, Class and Marks cannot be empty!";
     }
-}
 ?>
--- form.php
+++ form.php
@@ -1,4 +1,4 @@
-!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -50,3 +50,46 @@
 </body>
 
 </html>
+<section style="margin: 50px 0;">
+        <div class="container">
+            <table class="table table-dark">
+                <thead>
+                  <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Student Name</th>
+                    <th scope="col">Grade</th>
+                    <th scope="col">Marks</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        require_once "config.php";
+
+                        $sql_query = "SELECT * FROM results";
+                        if ($result = $conn ->query($sql_query)) {
+                            while ($row = $result -> fetch_assoc()) { 
+                                $Id = $row['id'];
+                                $Name = $row['name'];
+                                $Grade = $row['grade'];
+                                $Marks = $row['marks'];
+                    ?>
+                    
+                    <tr class="trow">
+                        <td><?php echo $Id; ?></td>
+                        <td><?php echo $Name; ?></td>
+                        <td><?php echo $Grade; ?></td>
+                        <td><?php echo $Marks; ?></td>
+                        <td><a href="updatedata.php?id=<?php echo $Id; ?>" class="btn btn-primary">Edit</a></td>
+                        <td><a href​="deletedata.php?id=<?php echo $Id; ?>" class="btn btn-danger">Delete</a></td>
+                    </tr>
+
+                    <?php
+                            } 
+                        } 
+                    ?>
+                </tbody>
+              </table>
+        </div>
+    </section>
